6422e209b2a65a0e29e50e162e6da7bea9e6a2bf
Claire_M <clairerodriguez79@yahoo.fr>
2023-05-19 14:56:57 +0200

    Rajout composant Item pour affichage nickel des données

--- next05-client-components/types.d.ts
+++ next05-client-components/types.d.ts
@@ -2,6 +2,7 @@ type Result = {
     pageid: string,
     title: string,
     extract: string,
+    //  thumbnail: une propriété optionnelle qui représente une miniature (image réduite) associée à la page. 
     thumbnail?: {
         source: string,
         width: number,
